8b5388e453e75d3e4808562293058f8c73a7c80d
Bubba Godinet <godinetvn@gmail.com>
2025-03-04 17:53:40 -0700

    updated process.env

--- lib/fetchData.jsx
+++ lib/fetchData.jsx
@@ -1,7 +1,6 @@
 import { getStoryblokApi } from '@/lib/storyblok';
 
 export async function fetchData() {
-  console.log("Fetching data with token:", process.env.NEXT_PUBLIC_STORYBLOK_TOKEN);
   const storyblokApi = getStoryblokApi();
   return storyblokApi.get('cdn/stories/home', { version: 'draft' });
 }
