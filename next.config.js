e8738f2d7b7e69ac1eecaa5d28ac206d193723a0
Bubba Godinet <godinetvn@gmail.com>
2025-03-04 18:49:25 -0700

    updated preview things

--- next.config.js
+++ next.config.js
@@ -44,6 +44,15 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: "/api/preview",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
     ];
   },
 };
