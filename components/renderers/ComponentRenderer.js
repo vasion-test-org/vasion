83dbd25601399bbfd9f42f78b9aa90524a2d0763
Tanner Davison <Tboydavison@gmail.com>
2025-04-04 10:32:10 -0600

    fixed side by side

--- components/renderers/ComponentRenderer.js
+++ components/renderers/ComponentRenderer.js
@@ -11,17 +11,31 @@ import { usePathname } from "next/navigation";
 import Button from "../globalComponents/Button";
 import Form from "../Form";
 import ContactCard from "../globalComponents/ContactCard";
+import OverviewController from "../overview/OverviewController";
+import SmallQuote from "components/SmallQuote";
+
 const ComponentRenderer = ({ blok }) => {
   if (!blok) return null;
-  const copycomponents = ['body_copy', 'header', 'eyebrow', 'long_form_text', 'copy_block'];
+  const copycomponents = [
+    "body_copy",
+    "header",
+    "eyebrow",
+    "long_form_text",
+    "copy_block",
+    "overview_controller",
+    "small_quote",
+  ];
   // console.log(blok);
 
   const pathname = usePathname();
   const isFrench = pathname.startsWith("/fr");
   const isGerman = pathname.startsWith("/de");
-  const isEnglish = !isFrench && !isGerman; 
-  
-  if (blok.component === "personalized_page" && Array.isArray(blok.personalized_section)) {
+  const isEnglish = !isFrench && !isGerman;
+
+  if (
+    blok.component === "personalized_page" &&
+    Array.isArray(blok.personalized_section)
+  ) {
     return (
       <SectionWrapper {...storyblokEditable(blok)}>
         {blok.personalized_section.map((section) => (
@@ -31,16 +45,15 @@ const ComponentRenderer = ({ blok }) => {
     );
   }
 
-
   if (blok.component === "personalized_section") {
     let contentBlocks = [];
 
     if (isEnglish && blok.english_blocks.length > 0) {
       contentBlocks = blok.english_blocks;
-    } 
+    }
     if (isFrench && blok.french_blocks.length > 0) {
       contentBlocks = blok.french_blocks;
-    } 
+    }
     if (isGerman && blok.german_blocks.length > 0) {
       contentBlocks = blok.german_blocks;
     }
@@ -66,11 +79,11 @@ const ComponentRenderer = ({ blok }) => {
         <CopyDiv>
           {blok?.copy_block_sections?.map((item, index) => (
             <BlockWrapper key={index} {...storyblokEditable(item)}>
-                  {copycomponents.includes(item.component) ? (
-                  <RichTextRenderer document={item.copy} blok={item}/>
-                ) : (
-                  <ComponentRenderer blok={item}/>
-                )}
+              {copycomponents.includes(item.component) ? (
+                <RichTextRenderer document={item.copy} blok={item} />
+              ) : (
+                <ComponentRenderer blok={item} />
+              )}
             </BlockWrapper>
           ))}
         </CopyDiv>
@@ -78,13 +91,21 @@ const ComponentRenderer = ({ blok }) => {
     case "logo_cube":
       return <LogoCube blok={blok} />;
     case "form":
-      return <Form blok={blok}/>
+      return <Form blok={blok} />;
     case "global_link":
-      return <Button $buttonData={blok}/>
-    case "contact_card": 
-    return <ContactCard blok={blok}/>
+      return <Button $buttonData={blok} />;
+    case "contact_card":
+      return <ContactCard blok={blok} />;
+    case "overview":
+      return <OverviewController blok={blok} />;
+    case "small_quote":
+      return <SmallQuote short blok={blok} />;
     default:
-      return <BlockWrapper {...storyblokEditable(blok)}>Unknown Component</BlockWrapper>;
+      return (
+        <BlockWrapper {...storyblokEditable(blok)}>
+          Unknown Component
+        </BlockWrapper>
+      );
   }
 };
 
@@ -94,15 +115,15 @@ const ImageWrapper = styled.div`
   ${media.fullWidth} {
     width: 512px;
   }
-  
+
   ${media.tablet} {
     width: 44.531vw;
   }
-  
+
   ${media.mobile} {
     min-width: 100%;
   }
-`
+`;
 const SectionWrapper = styled.section`
   display: flex;
   flex-direction: column;
@@ -140,7 +161,7 @@ const CopyDiv = styled.div`
     width: 41.016vw;
     gap: 1.563vw;
   }
-  
+
   ${media.mobile} {
     width: 89.167vw;
     gap: 3.333vw;
