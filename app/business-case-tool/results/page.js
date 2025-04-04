76a8d0aa2787c5ce30c47cfd51d88c49b4780b1b
Bubba Godinet <godinetvn@gmail.com>
2025-04-04 15:13:06 -0600

    bct fix

--- app/business-case-tool/results/page.js
+++ app/business-case-tool/results/page.js
@@ -1,50 +1,15 @@
+// app/business-case-tool/results/page.tsx
 'use client';
-import { useSearchParams } from 'next/navigation';
-import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
-import BCTPDF from 'components/BusinessCaseTool/BCTPDF';
 
-const BCTResults = () => {
-  const searchParams = useSearchParams();
+import React, { Suspense } from 'react';
+import ClientBCTResults from '@/components/BusinessCaseTool/ClientBCTResults';
 
-  const currency = searchParams.get('currency');
-  const rawData = searchParams.get('data');
-  let contactFormData, savingsFormData;
-
-  try {
-    const parsed = rawData ? JSON.parse(decodeURIComponent(rawData)) : {};
-    contactFormData = parsed.contactFormData;
-    savingsFormData = parsed.savingsFormData;
-    console.log(parsed)
-  } catch (e) {
-    console.error('Failed to parse data', e);
-  }
-
+const BCTResultsPage = () => {
   return (
-    <>
-      {contactFormData && savingsFormData && currency && 
-        <>
-          <BCTPDF
-            contactFormData={contactFormData}
-            savingsFormData={savingsFormData}
-            currency={currency}
-          />
-          <GlobalStyle />
-        </>
-  
-      }
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ClientBCTResults />
+    </Suspense>
   );
 };
 
-const GlobalStyle = createGlobalStyle`
-@media print {
-  body {
-    .cky-btn-revisit-wrapper {
-      display: none;
-    }
-  }
-}
-`;
-
-export default BCTResults;
+export default BCTResultsPage;
