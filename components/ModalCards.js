a2b28bed0014d9a0bcecd8a04404061c2b747b5f
Tanner Davison <Tboydavison@gmail.com>
2025-04-30 10:13:53 -0600

    removed unused imports

--- components/ModalCards.js
+++ components/ModalCards.js
@@ -2,7 +2,6 @@
 import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import media from "styles/media";
-import colors from "@/styles/colors";
 import text from "styles/text";
 import CardModal from "@/components/globalComponents/CardModal";
 import RichTextRenderer from "./renderers/RichTextRenderer";
