221dcab2610f017cdc38bc65ff8a1dc24dff417e
Tanner Davison <Tboydavison@gmail.com>
2025-04-30 12:13:06 -0600

    added card hover boolean

--- components/globalComponents/Card.js
+++ components/globalComponents/Card.js
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const Card = ({ content, paginated, borderradius }) => {
+  console.log("CARD-CONTENT", content);
   const pathname = usePathname();
   const buttonData = content.Button?.[0];
   const isEmail = buttonData?.link_url?.email;
@@ -20,10 +21,15 @@ const Card = ({ content, paginated, borderradius }) => {
   const rel = target === "_blank" ? "noopener noreferrer" : undefined;
 
   const isExternal = rawHref.startsWith("http");
-  const alreadyLocalized = rawHref.startsWith("/de") || rawHref.startsWith("/fr") || rawHref.startsWith("/en");
+  const alreadyLocalized =
+    rawHref.startsWith("/de") ||
+    rawHref.startsWith("/fr") ||
+    rawHref.startsWith("/en");
 
   const slugParts = pathname.split("/").filter(Boolean);
-  const currentLocale = ["de", "fr"].includes(slugParts[0]) ? slugParts[0] : null;
+  const currentLocale = ["de", "fr"].includes(slugParts[0])
+    ? slugParts[0]
+    : null;
 
   const normalizedUrl =
     isEmail || isExternal || alreadyLocalized
@@ -58,7 +64,12 @@ const Card = ({ content, paginated, borderradius }) => {
 
   return target !== "_blank" ? (
     <Link href={normalizedUrl} passHref legacyBehavior>
-      <CardWrapper as="a" {...storyblokEditable(content)} paginated={paginated}>
+      <CardWrapper
+        as="a"
+        {...storyblokEditable(content)}
+        paginated={paginated}
+        hashover={content?.has_hover}
+      >
         {WrapperContent}
       </CardWrapper>
     </Link>
@@ -69,6 +80,7 @@ const Card = ({ content, paginated, borderradius }) => {
       rel={rel}
       {...storyblokEditable(content)}
       paginated={paginated}
+      hashover={content?.has_hover}
     >
       {WrapperContent}
     </CardWrapper>
@@ -107,7 +119,6 @@ const ContentWrapper = styled.div`
     gap: 3.333vw;
   }
 `;
-
 const CardWrapper = styled.div`
   text-decoration: none;
   color: inherit;
@@ -124,6 +135,14 @@ const CardWrapper = styled.div`
   box-shadow:
     0vw 0vw 0.063vw 0vw rgba(25, 29, 30, 0.04),
     0vw 0.125vw 0.25vw 0vw rgba(25, 29, 30, 0.16);
+  transition: background 0.3s ease;
+
+  &:hover {
+    background: ${(props) =>
+      props.$hashover
+        ? "linear-gradient(180deg, #7E5FDD 0%, #583F99 100%)"
+        : "unset"};
+  }
 
   ${media.fullWidth} {
     gap: 16px;
