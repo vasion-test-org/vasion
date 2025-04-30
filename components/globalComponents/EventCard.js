4315e3e81fe226998dc85c4f23868fd95bde56a8
Bubba Godinet <godinetvn@gmail.com>
2025-04-30 16:34:22 -0600

    fixed pagination, hid filters and made the search bar optional, updated event cards css

--- components/globalComponents/EventCard.js
+++ components/globalComponents/EventCard.js
@@ -39,22 +39,41 @@ const EventCard = ({ content, even }) => {
 const DetailIcon = styled.img`
   width: 1.25vw;
   height: 1.25vw;
+
+  ${media.mobile} {
+    width: 4.167vw;
+    height: 4.167vw;
+  }
 `;
 const DetailDiv = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 0.5vw;
+  gap: 0.5vw; 
+
+  ${media.mobile} {
+    gap: 1.667vw;
+  }
 `;
 const Details = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5vw;
+
+  ${media.mobile} {
+    gap: 1.667vw;
+  }
 `;
 const DetailsContainer = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
   justify-content: space-between;
+
+  ${media.mobile} {
+    flex-direction: column;
+    gap: 1.667vw;
+  }
+
 `;
 const HeaderContainer = styled.div`
   display: flex;
@@ -66,25 +85,57 @@ const EventContent = styled.div`
   flex-direction: column;
   width: 18.188vw;
   gap: 0.5vw;
+
+  ${media.mobile} {
+    width: 100%;
+      gap: 1.667vw;
+  }
 `;
 const EventImage = styled.img`
   width: 3.625vw;
   height: 3.625vw;
+
+  ${media.mobile} {
+    display: none;
+  }
 `;
 const EventInfoContainer = styled.div`
   display: flex;
   align-items: center;
   flex-direction: row;
   gap: 1vw;
+
+  ${media.mobile} {
+    flex-direction: column;
+    gap: 3.333vw;
+  }
 `;
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 2vw 1.5vw;
-  width: 100%;
-  gap: 16.25vw;
   background: ${(props) =>
     props.even ? colors.lightPurpleGrey : colors.white};
+    width: 100%;
+  padding: 2vw 1.5vw;
+  gap: 16.25vw;
+
+  ${media.fullWidth} {
+    padding: 32px 24px;
+    gap: 260px;
+  }
+
+  ${media.tablet} {
+      padding: 3.125vw 2.344vw;
+    gap: 25.391vw;
+  }
+
+  ${media.mobile} {
+    align-items: flex-start;
+    flex-direction: column;
+    padding: 6.667vw 5vw;
+    gap: 3.333vw;
+  }
+    
 `;
 export default EventCard;
