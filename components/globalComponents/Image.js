e9850abd57429bd8472c2424b530325decd682d6
Tanner Davison <Tboydavison@gmail.com>
2025-05-01 17:02:05 -0600

    added in svgs

--- components/globalComponents/Image.js
+++ components/globalComponents/Image.js
@@ -30,11 +30,11 @@ const Image = ({ images, borderradius, filename }) => {
 const ImageWrapper = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: contain;
+  object-fit: cover;
   display: block;
   /* max-width: inherit;  */
   border-radius: ${(props) => `${props.borderradius || 0}px`};
-  
+
   ${media.fullWidth} {
     border-radius: ${(props) => `${props.borderradius || 0}px`};
   }
