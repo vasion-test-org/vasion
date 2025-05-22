0e9ff7eb76ac6fda7068d56d94a59882cad00a1a
Bubba Godinet <godinetvn@gmail.com>
2025-05-22 12:53:12 -0600

    made separate client component to handle marketo scripts

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ import Config from '@/components/Config';
 import { getStoryblokApi } from '@/lib/storyblok';
 import { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/next';
+import MarketoScripts from '@/components/MarketoScripts';
 
 export const metadata = {
   metadataBase: new URL('https://vasion.com'),
@@ -29,53 +30,13 @@ export default async function RootLayout({ children }) {
           name='google-site-verification'
           content='9aTxhC978Sh5yhlRXic1mj23gCh4RcexRTfgiwMKbks'
         />
-        {/* Marketo Munchkin */}
-        <Script
-          id='marketo-munchkin'
-          strategy='afterInteractive'
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function() {
-        var didInit = false;
-        function initMunchkin() {
-          if(didInit === false) {
-            didInit = true;
-            Munchkin.init('338-HTA-134');
-          }
-        }
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = '//munchkin.marketo.net/munchkin.js';
-        s.onreadystatechange = function() {
-          if (this.readyState == 'complete' || this.readyState == 'loaded') {
-            initMunchkin();
-          }
-        };
-        s.onload = initMunchkin;
-        document.getElementsByTagName('head')[0].appendChild(s);
-      })();
-    `,
-          }}
-        />
+        <MarketoScripts />
 
         {/* CookieYes */}
         <Script
           id='cookieyes'
           strategy='afterInteractive'
           src='https://cdn-cookieyes.com/client_data/c1cc367c126e833f0301eb2c/script.js'
-        />
-
-        {/* Marketo Forms2 */}
-        <Script
-          id='marketo-forms'
-          strategy='afterInteractive'
-          src='https://info.printerlogic.com/js/forms2/js/forms2.min.js'
-          onLoad={() => {
-            if (typeof window !== 'undefined') {
-              window.MktoForms2 = window.MktoForms2 || {};
-            }
-          }}
         />
 
         {/* Intercom */}
