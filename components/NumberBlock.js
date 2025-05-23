aa6a6124edd7281aaee92e58815abff7528b3176
Tanner Davison <Tboydavison@gmail.com>
2025-05-22 22:46:09 -0600

    made number blok editable

--- components/NumberBlock.js
+++ components/NumberBlock.js
@@ -1,52 +1,57 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 
-import styled from 'styled-components';
-import { storyblokEditable } from '@storyblok/react/rsc';
-import media from '@/styles/media';
-import text from '@/styles/text';
-import RichTextRenderer from '@/components/renderers/RichTextRenderer';
+import styled from "styled-components";
+import { storyblokEditable } from "@storyblok/react/rsc";
+import media from "@/styles/media";
+import text from "@/styles/text";
+import RichTextRenderer from "@/components/renderers/RichTextRenderer";
 
 const NumberBlock = ({ blok }) => {
   // console.log(blok)
-return (
-<NumberBlockContainer>
- <NumberBlockDiv bg={blok.block_color.value} color={blok.text_color.value}>{blok.number}</NumberBlockDiv>
- <RichTextRenderer key={`copy-`} document={blok.header[0].copy} blok={blok}/>
-</NumberBlockContainer>
-)
-}
+  return (
+    <NumberBlockContainer {...storyblokEditable(blok.header[0])}>
+      <NumberBlockDiv bg={blok.block_color.value} color={blok.text_color.value}>
+        {blok.number}
+      </NumberBlockDiv>
+      <RichTextRenderer
+        key={`copy-`}
+        document={blok.header[0].copy}
+        blok={blok}
+      />
+    </NumberBlockContainer>
+  );
+};
 
 const NumberBlockDiv = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-color: ${props => props.color ? `${props.color}` : 'unset'};
-background: ${props => props.bg ? `${props.bg}` : 'unset'};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: ${(props) => (props.color ? `${props.color}` : "unset")};
+  background: ${(props) => (props.bg ? `${props.bg}` : "unset")};
   width: 4.5vw;
   height: 4.5vw;
   border-radius: 0.5vw;
 
   ${media.fullWidth} {
     width: 72px;
-  height: 72px;
-  border-radius: 8px;
-
+    height: 72px;
+    border-radius: 8px;
   }
-  
+
   ${media.tablet} {
     width: 7.031vw;
-  height: 7.031vw;
-  border-radius: 0.781vw;
+    height: 7.031vw;
+    border-radius: 0.781vw;
   }
-  
+
   ${media.mobile} {
     width: 15vw;
-  height: 15vw;
-  border-radius: 1.667vw;
+    height: 15vw;
+    border-radius: 1.667vw;
   }
-`
+`;
 const NumberBlockContainer = styled.div`
   ${text.h3};
   display: flex;
@@ -59,15 +64,16 @@ const NumberBlockContainer = styled.div`
     gap: 32px;
     padding: 30px 0;
   }
-  
+
   ${media.tablet} {
     gap: 3.125vw;
     padding: 2.93vw 0;
   }
-  
+
   ${media.mobile} {
     gap: 6.667vw;
     padding: 6.25vw 0;
   }
 `;
-export default NumberBlock
+export default NumberBlock;
+
