1c263b02056e3c04454456040c97c6aca4972f01
Tanner Davison <Tboydavison@gmail.com>
2025-06-23 16:27:11 -0600

    removed console log

--- components/globalComponents/Card.js
+++ components/globalComponents/Card.js
@@ -9,7 +9,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const Card = ({ content, paginated, borderradius }) => {
-  // console.log(content);
   const pathname = usePathname();
   const buttonData = content.Button?.[0];
   const isEmail = buttonData?.link_url?.email;
