3da9285e6878360c7a2cf117202c68afe6f89850
Bubba Godinet <godinetvn@gmail.com>
2025-07-29 11:56:48 -0600

    review cards done

--- components/centeredSections/ReviewCtaCards.js
+++ components/centeredSections/ReviewCtaCards.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import RichTextRenderer from '../renderers/RichTextRenderer';
 import colors from '@/styles/colors';
 import Button from '@/components/globalComponents/Button';
+import media from '@/styles/media';
 
 const ReviewCtaCards = ({ blok }) => {
   console.log(blok);
@@ -35,6 +36,18 @@ const RatingDiv = styled.div`
   flex-direction: row;
   justify-content: space-between;
   margin-top: 2vw;
+
+  ${media.fullWidth} {
+    margin-top: 32px;
+  }
+
+  ${media.tablet} {
+    margin-top: 3.125vw;
+  }
+
+  ${media.mobile} {
+    margin-top: 6.667vw;
+  }
 `;
 
 const ReviewHeader = styled.div`
@@ -49,8 +62,20 @@ const ReviewIcon = styled.img`
 const ReviewHeaderDiv = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 0.5vw;
   align-items: center;
+  gap: 0.5vw;
+
+  ${media.fullWidth} {
+    gap: 8px;
+  }
+
+  ${media.tablet} {
+    gap: 0.781vw;
+  }
+
+  ${media.mobile} {
+    gap: 1.667vw;
+  }
 `;
 
 const ReviewCard = styled.div`
@@ -58,12 +83,33 @@ const ReviewCard = styled.div`
   display: flex;
   flex-direction: column;
   background: ${colors.darkPurple};
-  border-radius: 1.25rem;
-  padding: 1.5rem 2rem 1.25rem 2rem;
-  width: 25.813rem;
-  min-height: 8.25rem;
+  border-radius: 1.25vw;
+  padding: 1.5vw 2vw 1.25vw 2vw;
+  width: 25.813vw;
+  min-height: 8.25vw;
   transition: background 0.3s ease;
   background-blend-mode: hard-light, hard-light, normal;
+
+  ${media.fullWidth} {
+    width: 413px;
+    min-height: 132px;
+    border-radius: 20px;
+    padding: 24px 32px 20px 32px;
+  }
+
+  ${media.tablet} {
+    width: 28.613vw;
+    min-height: 12.891vw;
+    border-radius: 1.953vw;
+    padding: 2.344vw 3.125vw 1.953vw 3.125vw;
+  }
+
+  ${media.mobile} {
+    width: 89.167vw;
+    min-height: 27.5vw;
+    border-radius: 4.167vw;
+    padding: 5vw 6.667vw 4.167vw 6.667vw;
+  }
 
   &:hover {
     background: linear-gradient(
@@ -84,6 +130,19 @@ const ReviewCardWrapper = styled.div`
   display: flex;
   flex-direction: row;
   gap: 2vw;
+
+  ${media.fullWidth} {
+    gap: 32px;
+  }
+
+  ${media.tablet} {
+    gap: 3.125vw;
+  }
+
+  ${media.mobile} {
+    flex-direction: column;
+    gap: 6.667vw;
+  }
 `;
 
 export default ReviewCtaCards;
