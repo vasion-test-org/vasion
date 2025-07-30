4237312eaed9c5353053159e123e1bea224c6114
Bubba Godinet <godinetvn@gmail.com>
2025-07-30 11:06:25 -0600

    review cta done

--- components/Hero.js
+++ components/Hero.js
@@ -86,13 +86,13 @@ const Hero = ({ blok }) => {
                 />
               </div>
             ))}
-            {blok.badges && (
+            {/* {blok.badges && (
               <div>
                 {blok.badges.map((badge) => (
                   console.log(badge)
                 ))}
               </div>
-            )}
+            )} */}
             {!blok?.socials && (
               <ButtonRow>
                 {blok?.button_group?.map(($buttonData) => (
