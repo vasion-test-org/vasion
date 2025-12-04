cc0f2c59ce2875d0e415ee4e446d8e88d6bd1e35
Tanner Davison <tanner.davison95@gmail.com>
2025-12-04 13:41:31 -0700

    testing

--- components/centeredSections/Grid_tw.js
+++ components/centeredSections/Grid_tw.js
@@ -1,13 +1,13 @@
 "use client";
 import React from "react";
 
-import GridItem from "@/components/globalComponents/GridItem";
+import GridItem_tw from "@/components/globalComponents/GridItem_tw";
 
 const Grid_tw = ({ gridData, alignment }) => {
   return (
     <div className="flex flex-row flex-wrap items-start justify-center w-[81.5vw] gap-[2.5vw] fullWidth:w-[1304px] fullWidth:gap-10 tablet:w-[92.188vw] tablet:gap-[1.953vw] mobile:w-[89.167vw] mobile:gap-[8.333vw]">
       {gridData.map((gridItem, index) => (
-        <GridItem
+        <GridItem_tw
           key={gridItem._uid || `grid-item-${index}`}
           content={gridItem}
           alignment={alignment}
