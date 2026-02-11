1c6c13c9c612a4eeb38162ccec297b71039ff76a
Tanner <tanner@vasion.com>
2026-02-11 15:40:52 -0700

    adding global file

--- types/global.d.ts
+++ types/global.d.ts
@@ -5,5 +5,12 @@ declare global {
     MktoForms2?: {
       whenReady: (callback: (form: any) => void) => void;
     };
+    grecaptcha?: {
+      render: (container: Element, params: Record<string, unknown>) => number;
+      getResponse: (widgetId?: number) => string;
+      execute: (widgetId?: number) => void;
+      reset: (widgetId?: number) => void;
+    };
+    onRecaptchaLoaded?: () => void;
   }
 }
